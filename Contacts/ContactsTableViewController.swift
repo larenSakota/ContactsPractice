508c91c52c3b8f1e18f646c8ec135cf1b2381771
larenSakota <jedi.temple@yahoo.com>
2017-03-11 20:15:59 -0800

    debugged the issue

--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -30,15 +30,15 @@ class ContactsTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         // Configure the cell...
         cell.textLabel?.text = "A fine example of a UITableViewCell."
         return cell

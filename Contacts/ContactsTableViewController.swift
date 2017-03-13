33f11e0013f82ecab4863bfffb6094baae9410d6
larenSakota <jedi.temple@yahoo.com>
2017-03-12 20:49:25 -0700

    finished first section.  need more practice

--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -14,12 +14,16 @@ class ContactsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let sheilla = Contact(phoneNumber: "867-5309")
+        let laren = Contact(name: "Laren", phoneNumber: "888-888-8888")
+        let ging = Contact(name: "Ging")
+        
+        self.contacts.append(sheilla)
+        self.contacts.append(laren)
+        self.contacts.append(ging)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,11 +34,11 @@ class ContactsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return self.contacts.count
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.contacts.count
     }
     
     
